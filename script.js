fdcdf825c68d578fe383d25a2c734888ea7ea38b
Elaine Calasans <42349929+lancalasans@users.noreply.github.com>
2020-03-23 12:09:22 -0300

    CallBacks e Promisses os Conceitos

--- script.js
+++ script.js
@@ -1,35 +1,84 @@
-//EventEmitter
+//PROMISES 
 
-const EventEmitter = require('events');
-
-class Users extends EventEmitter {
-    userLogged(data){
-        setTimeout(() => {
-            this.emit ('User logged', data);
-        }, 2000);
-       
-    }
-}
-
-const users = new Users();
-
-emitter.on('User logged', data =>{
- console.log(data);
+const doSomethingPromise = () => 
+new Promise((resolve, reject => {
+   // throw new Error("Something wrong");
+    setTimeout(function() {
+        resolve('First data');
+    }, 1000);
 });
 
-Users.userLogged({ user: 'Elaine Souza'});
-Users.userLogged({ user: 'Souza Elaine'});
+const doOtherthingPromise = () =>
+ new Promise((resolve, reject => {
+     throw new Error('Ops, Erro');
+    setTimeout(function() {
+        resolve('Second data');
+    }, 1000);
+});
 
-// EventTarget
+Promise.reca([doSomethingPromise(), doOtherThingPromise()]).then(data => {
+console.log(data);
+});
 
-const EventEmitter = require('events');
+//-----------------//-----------------//
+//Promise.all([doSomethingPromise(), doOtherthingPromise()]).then((data) => {
+   // console.log(data[0].split(''));
+    //console.log(data[0].split(''));
+//}) .catch(err => {
+  //  console.log(err)
+//});
+//----------------//-----------------//
+// doSomethingPromise()
+//.then(data => {
+  //  consolo.log(data.split(''));
+   // return doOtherThingPromise;
+//})
+//.then(data2 => console.log(data2.split('')))
+//.catch(error => console.log('Ops, erro', error));
 
-class Users extends EventEmitter {
-    userLogged(data){
-        setTimeout(() => {
-            this.emit ('User logged', data);
-        }, 2000);
-       
-    }
+
+// Pending
+// Fulfilled
+// Rejected 
+
+//----------------------------------//
+//CALLBACKS
+function toSomething(callback) {
+    setTimeout(function() {
+        //did something
+        callback('First data');
+    }, 1000);
 }
 
+function doOtherThing(callback) {
+    setTimeout(function() {
+        //did other thing
+        callback('Second data');
+    }, 1000);
+}
+
+function doAll() {
+    try {
+          doSomething(function(data) {
+            var processedData = data.spplit('');
+
+                try{
+                doOtherThing(function(data2) {
+                    var processedData2 = data2.split('');
+        
+                    try {
+                 setTimeout(function() {
+                    console.log(processedData, processedData2);
+             },  1000);
+            } catch(err) {           
+            }  
+        });   
+ } catch(err) {
+     //handle error
+ }
+});
+} catch (err) {
+    //handle error
+  }
+}
+doAll();
