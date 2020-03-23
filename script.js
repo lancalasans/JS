4cde4b61a08de34781b49fd2ab42e84211071ee1
Elaine Calasans <42349929+lancalasans@users.noreply.github.com>
2020-03-23 12:10:17 -0300

    Debuggin2

--- script.js
+++ script.js
@@ -1,84 +1,24 @@
-//PROMISES 
+// Console
 
-const doSomethingPromise = () => 
-new Promise((resolve, reject => {
-   // throw new Error("Something wrong");
-    setTimeout(function() {
-        resolve('First data');
-    }, 1000);
-});
+// console.log('Black text');
+//console.war('Yellow text with alert');
+//console.error('red error text');
 
-const doOtherthingPromise = () =>
- new Promise((resolve, reject => {
-     throw new Error('Ops, Erro');
-    setTimeout(function() {
-        resolve('Second data');
-    }, 1000);
-});
+//console.trace();
 
-Promise.reca([doSomethingPromise(), doOtherThingPromise()]).then(data => {
-console.log(data);
-});
+/*
+console.group('My group');
+console.log('Info inside group');
+console.log('More info inside group');
+console.groupEnd ('My froup');
+*/
 
-//-----------------//-----------------//
-//Promise.all([doSomethingPromise(), doOtherthingPromise()]).then((data) => {
-   // console.log(data[0].split(''));
-    //console.log(data[0].split(''));
-//}) .catch(err => {
-  //  console.log(err)
-//});
-//----------------//-----------------//
-// doSomethingPromise()
-//.then(data => {
-  //  consolo.log(data.split(''));
-   // return doOtherThingPromise;
-//})
-//.then(data2 => console.log(data2.split('')))
-//.catch(error => console.log('Ops, erro', error));
+console.time('Log time');
+setTimeout(() => {
+    console.timeEnd('Log time');
+}, 2000);
 
+// console.table(['Celso Henrique', 'Digital Innovation One']);
+console.assert(1 === 0, 'Ops'); 
 
-// Pending
-// Fulfilled
-// Rejected 
-
-//----------------------------------//
-//CALLBACKS
-function toSomething(callback) {
-    setTimeout(function() {
-        //did something
-        callback('First data');
-    }, 1000);
-}
-
-function doOtherThing(callback) {
-    setTimeout(function() {
-        //did other thing
-        callback('Second data');
-    }, 1000);
-}
-
-function doAll() {
-    try {
-          doSomething(function(data) {
-            var processedData = data.spplit('');
-
-                try{
-                doOtherThing(function(data2) {
-                    var processedData2 = data2.split('');
-        
-                    try {
-                 setTimeout(function() {
-                    console.log(processedData, processedData2);
-             },  1000);
-            } catch(err) {           
-            }  
-        });   
- } catch(err) {
-     //handle error
- }
-});
-} catch (err) {
-    //handle error
-  }
-}
-doAll();
+// console.log('%c styled log"), color: blue;');
